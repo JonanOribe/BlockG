7a29f1f1cc489192d7204933233c0916d0a63075
UnexpectedCharacter <cybernote11@gmail.com>
2021-05-27 15:24:21 +0200

    Back end  nuevas implementaciones

--- README.md
+++ README.md
@@ -7,20 +7,28 @@
 <h3>:mag_right: Testing</h3>
 <li>Lanzamos los tests con <b>Blablabla</b></li>
 <h3>:rocket: Lanzamos el proyecto con:</h3>
-<<<<<<< Updated upstream
+
 <li>Para la versión móvil: <b>ng serve</b> en mobile_QR_app</li>
 <li>Para el BackEnd: <b>node app.js</b> en backEnd-node</li>
-=======
-<li>En REMIX nos conectamos a Ganache</li>
+
+<h3>Roadmap para lanzar prueba completa</h3>
+<li>Ejecutamos e iniciamos una red Ethereum con Ganache</li>
+<li>En REMIX copiamos el contenido del contrato qr.sol en el IDE y nos conectamos a Ganache mediante el puerto 7545 usando un WEB3 Provider</li>
 <li>Compilamos el contrato</li>
-<li>Desplegamos el contrato con los siguientes datos: "Champions League", "1621940291"</li>
-<li>Llamamos a la funcion crearEvento("Chelsea-Manchester City", 1621940485)</li>
-<li>Abrimos el fichero app.js y cambiamos el address del contrato desplegado</li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
->>>>>>> Stashed changes
+<li>Desplegamos el contrato con los siguientes datos: "Villareal - Manchester United", "1621940291", "20"</li>
+<li>En la parte del back end en el fichero app.js establecemos el address del contrato desplegado en la variable QRContract</li>
+<li>En la variable platformMainAddr establecemos el address que haya desplegado el contrato</li>
+<li>Llamamos a la peticion requestNewAccount pasandole el JSON como aparece en la documentacion de la funcion</li>
+<li>Llamamos a la peticion getEventPrice para obtener el precio del evento</li>
+<li>Llamamos a la peticion requestTicket pasandole el JSON como aparece en la documentacion de la funcion, a tener en cuenta que los datos de usuario sean los devueltos por la anterior peticion</li>
+<li>Ya tendriamos una entrada comprada</li>
+
 <h3>:blue_book: Diagrama del proyecto:</h3>
 
 ![alt text](https://github.com/JonanOribe/BlockG/blob/main/docs/BlockG.png?raw=true)
+
+
+
+
+<li>Para la versión móvil: <b>ng serve</b> en mobile_QR_app</li>
+<li>Para el BackEnd: <b>node app.js</b> en backEnd-node</li>
--- backEnd-node/app.js
+++ backEnd-node/app.js
@@ -8,10 +8,9 @@ app.use(cors());
 
 const Web3 = require("web3");
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-const QRContractABI = [{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"name":"compraEntrada","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"uint256","name":"_fecha","type":"uint256"}],"name":"creaEvento","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"uint256","name":"_fecha","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"receive"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"asistente","outputs":[{"internalType":"string","name":"nombre","type":"string"},{"internalType":"string","name":"email","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"asistentes","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"asistentesLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"eventos","outputs":[{"internalType":"string","name":"nombre","type":"string"},{"internalType":"uint256","name":"fecha","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fecha","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nombre","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"whoAmI","outputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"stateMutability":"view","type":"function"}]
-const QRContract = new web3.eth.Contract(QRContractABI, '0xe3A376D67011739894Fe944B716DDa91E57E2C89');
-
-var accountsCounter = 0;
+const QRContractABI = [{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"name":"compraEntrada","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"uint256","name":"_fecha","type":"uint256"},{"internalType":"uint256","name":"_cost","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"receive"},{"inputs":[],"name":"admin","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"asistente","outputs":[{"internalType":"string","name":"nombre","type":"string"},{"internalType":"string","name":"email","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"asistentes","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"asistentesLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fecha","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nombre","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"whoAmI","outputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"stateMutability":"view","type":"function"}];
+const QRContract = new web3.eth.Contract(QRContractABI, '0xBE6B769b3Fce00357ACb9D7564A33f0B5c060899'); //Change in first execution
+let platformMainAddr = '0xcE9e86FFaCF9541756fB70adD5fe6e04d6241dE4'; //Change in first execution
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,23 +18,59 @@ app.use(methodOverride());
 
 var router = express.Router();
 
+/**
+ * Solicita una peticion de compra 
+ * 
+ * Request Body
+ * {
+ * "address": "0x5C3c837B9d1E6C5B18E98F51e8E5aa265b37f0DC",
+ * "password": "!@superpassword",
+ * "eventName": "Champions League",
+ * "email": "test66@gmail.com",
+ * "price": 230
+ * }
+ */
 router.post("/requestTicket", async function (req, res) {
-    console.log(req.body);
+    //Unlock account
+    web3.eth.personal.unlockAccount(req.body.address, req.body.password, 15000);
+    //Send transaction
+    var returnedValue = await QRContract.methods.compraEntrada(req.body.eventName, req.body.email).send({ from: req.body.address, value: req.body.price, gas:3000000});
 
-    const accounts = await web3.eth.getAccounts();
-    let platformMainAddr = accounts[accountsCounter];
-    var returnedValue = await QRContract.methods.compraEntrada("Chelsea-Manchester City", "test@gmail.com").send({ from: platformMainAddr, gas:3000000});
+    if (returnedValue.status === true) {
+      res.status(200).send(JSON.stringify('GREEN'));
 
-    const SCResponse = JSON.stringify(returnedValue);
-
-    if (SCResponse.status === true) {
-      res.send("GREEN");
     } else {
-      res.send("RED");
+      res.status(500).send(JSON.stringify('RED'));
     }
-
-    accountsCounter++;
   });
+
+  /**
+   * Genera una nueva cuenta que se le anade 5 ether
+   *  
+   * RequestBody
+   * {
+   *  "password" : "!@superpassword"
+   * }
+   */
+  router.post("/requestNewAccount", async function (req, res) {
+    (async () => {
+      let newAccount = await web3.eth.personal.newAccount(req.body.password);
+      web3.eth.sendTransaction({from: platformMainAddr, to:newAccount, value: web3.utils.toWei('5', 'ether'), gasLimit: 21000, gasPrice: 20000000000});
+
+      const response = {"address" : newAccount};
+      res.status(200).send(JSON.stringify(response));
+    })();
+  });
+
+
+  /**
+   * Obtener el precio del evento
+   */
+router.get("/getEventPrice", async function (req, res) {
+  var returnedValue = await QRContract.methods.cost().call();
+
+    res.status(200).send(JSON.stringify(returnedValue));
+});
 
 app.use(router);
 
