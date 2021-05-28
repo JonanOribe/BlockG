514b4aa4e633e3fa26f36bd646d75ed58c48934b
UnexpectedCharacter <cybernote11@gmail.com>
2021-05-28 10:30:40 +0200

    Servicio para obtencion de ether de cuenta

--- backEnd-node/app.js
+++ backEnd-node/app.js
@@ -9,8 +9,8 @@ app.use(cors());
 const Web3 = require("web3");
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 const QRContractABI = [{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"name":"compraEntrada","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"uint256","name":"_fecha","type":"uint256"},{"internalType":"uint256","name":"_cost","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"receive"},{"inputs":[],"name":"admin","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"asistente","outputs":[{"internalType":"string","name":"nombre","type":"string"},{"internalType":"string","name":"email","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"asistentes","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"asistentesLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fecha","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nombre","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"whoAmI","outputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"stateMutability":"view","type":"function"}];
-const QRContract = new web3.eth.Contract(QRContractABI, '0x0633cE19AA842769160d4CA31026BB80151e2b53'); //Change in first execution
-let platformMainAddr = '0xfC459bA31941E90ef566324275862c439ac170D9'; //Change in first execution
+const QRContract = new web3.eth.Contract(QRContractABI, '0xAb3E5C07F4d86e340482a7C33456ba657CE03eE0'); //Change in first execution
+let platformMainAddr = '0xa6b3D457b5f701DAae9a71e44E00cFe3e80fB479'; //Change in first execution
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -72,6 +72,19 @@ router.get("/getEventPrice", async function (req, res) {
 
     res.status(200).send(JSON.stringify(returnedValue));
 });
+
+  /**
+   * Obtener el precio del evento
+   * 
+   * URL Path
+   * /getAddressEthBalance/0xBDe64338b109F5C144aa66a484f068fF094f2072
+   */
+   router.get("/getAddressEthBalance/:address", async function (req, res) {
+    let returnedValue = await web3.eth.getBalance(req.params.address);
+    let convertedValueFromWei = web3.utils.fromWei(returnedValue , 'ether')
+  
+    res.status(200).send(JSON.stringify(convertedValueFromWei));
+  });
 
 app.use(router);
 
